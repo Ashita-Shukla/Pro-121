dcc1851588c25c38ddbd12eb1506f51097e2548e
Ashita-Shukla <77487423+Ashita-Shukla@users.noreply.github.com>
2022-07-20 15:21:40 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,41 @@
+import cv2
+import time
+import numpy as np
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+output_file = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+
+cap = cv2.VideoCapture(0)
+time.sleep(2)
+bg = 0
+
+for i in range(60):
+  ret, bg = cap.read()
+
+bg = np.flip(bg, axis=1)
+
+while(cap.isOpened()):
+    ret, img = cap.read()
+    if not ret:
+        break
+
+    img = np.flip(img, axis = 1)
+
+    frame = cv2.resize(frame, (640, 480))
+    image = cv2.resize(image, (640, 480))
+
+    u_black = np.array([104, 153, 70])
+    l_black = np.array([30, 30, 0])
+    mask = cv2.inRange(frame, u_black, l_black)
+
+    #mask_1 = cv2.morphologyEx(mask_1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
+    #mask_1 = cv2.morphologyEx(mask_1, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8))
+
+    res_1 = cv2.bitwise_and(frame, frame, mask = mask)
+
+    f = frame - res_1
+    f = np.where(f == 0, image, f)
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
